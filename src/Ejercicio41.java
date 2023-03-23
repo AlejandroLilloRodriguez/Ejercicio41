a8a7f9b2f402d230824d834d39f39d1f2a7ed913
AlejandroLilloRodriguez <alejandro.lillorodriguez@usp.ceu.es>
2023-03-23 16:24:55 +0100

    Ejercicio41

--- src/Ejercicio41.java
+++ src/Ejercicio41.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Ejercicio41 {
+public class Ejercicio41 {
+    public static boolean buscar(int e, int[] array){
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == e) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean buscar1(int e, int[] array){
+        for (int i = array.length; i <0; i--) {
+            if (array[i] == e) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean buscar2(int e, int[] array){
+        int i=array.length/2;
+        while (i<array.length && i>=0) {
+            if (array[i] == e) {
+                return true;
+            }
+            if (array[i] < e) {
+                i++;
+            }else{
+                i--;
+            }
+
+        }
+        return false;
+    }
 }
